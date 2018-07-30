829759b9d920e075ae2707563ccebb480c8ce2d6
Kristina Tong <kristina.tong@gmail.com>
2018-07-30 17:39:21 +0000

    automatically backed up by learn

--- countdown.rb
+++ countdown.rb
@@ -6,11 +6,11 @@ def countdown(int)
   'HAPPY NEW YEAR!'
 end
 
-def countdown_with_sleep
+def countdown_with_sleep(int)
   while int > 0
-    sleep 1
     puts "#{int} SECOND(S)!"
     int -= 1
+    sleep 1
   end
   'HAPPY NEW YEAR!'
 end
